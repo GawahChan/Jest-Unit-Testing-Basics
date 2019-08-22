4dccf039b8b76a0f165d54b011f45c63dcebdf01
Gawah Chan <gawahchan@gmail.com>
2019-08-22 22:00:06 +0100

    Basic unit tests using basic jest matchers pt 2

--- functions.test.js
+++ functions.test.js
@@ -32,3 +32,27 @@ test('To be falsy', () => {
 test('User should return Gawah Chan object', () => {
     expect(functions.createUser()).toEqual({'firstName': 'Gawah', 'lastName': 'Chan'});
 });
+
+// Less than and greater than:
+// toBeLessThan()
+// toBeLessThanOrEqual()
+// toBeGreaterThan()
+// toBeGreaterThanOrEqual()
+
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600);
+});
+
+// Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/i);
+});
+
+// Arrays
+// Arrays can also use .toContain()
+test('Admin should be in userNames', () => {
+    userNames = ['John', 'Karen', 'admin'];
+    expect(userNames).toContain('admin');
+});
